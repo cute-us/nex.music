837f74f7e0052e5af37c2f5b0e5d4764f03e8a02
cute-us <163985378+cute-us@users.noreply.github.com>
2024-06-17 12:36:21 +0530

    💙💙

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/cute-us/nex.music",
+    "https://github.com/cute-us/nex.music.",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
